ccb5892d76cece5045ff8a08510d409ff2951c64
sakkarin1202 <xfake12340@hotmail.com>
2024-06-23 14:24:54 +0700

    first commit

---
+++ Grab/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Component/Header'
+import Search from './Component/Search'
+import Restaurant from './Component/Restaurant'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='container flex flex-col items-center p-4 mx-auto space-y-6'>
+        <Header/>
+        <Search/>
+        <Restaurant/>
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ Grab/src/Component/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1 className='text-xl'>Grab Restaurant</h1>
+  )
+}
+
+export default Header
